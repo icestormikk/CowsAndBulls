e47f0c79f1a88e59b6c429598a9443971ed1f893
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-09 20:07:02 +0300

    Added several new screens and laid the foundations for writing business logic

---
+++ src/main/kotlin/com/example/fragments/StartScreen.kt
@@ -0,0 +1,48 @@
+package com.example.fragments
+
+import javafx.scene.Parent
+import javafx.scene.control.Label
+import javafx.scene.layout.Border
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.BorderStroke
+import javafx.scene.layout.BorderStrokeStyle
+import javafx.scene.layout.BorderWidths
+import javafx.scene.layout.CornerRadii
+import javafx.scene.layout.FlowPane
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import org.kordamp.ikonli.javafx.FontIcon
+import tornadofx.*
+
+
+class StartScreen: Fragment("StartScreen") {
+    override val root: Parent by fxml()
+
+    private val rulesScreen = find(RulesScreen::class)
+    private val redirectToRulesLabel: Label by fxid()
+    private val gamemodePanelComputer: FlowPane by fxid()
+    private val gamemodePanelHuman: FlowPane by fxid()
+
+    init {
+        redirectToRulesLabel.onLeftClick {
+            (root.parent.parent as BorderPane).center.replaceChildren(rulesScreen.root)
+        }
+        // Computer
+        gamemodePanelComputer.children.add(1, FontIcon("cib-probot"))
+        // Human
+        gamemodePanelHuman.children.add(0, FontIcon("cil-people"))
+
+        listOf(gamemodePanelComputer, gamemodePanelHuman).forEach { flowPane ->
+            with(flowPane.parent as VBox) {
+                onHover {
+                    border = if (it) Border(BorderStroke(
+                        Color.DARKGREEN,
+                        BorderStrokeStyle.SOLID,
+                        CornerRadii(2.0),
+                        BorderWidths(1.0)
+                    )) else Border.EMPTY
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/fragments/StatisticsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.fragments
+
+import javafx.scene.Parent
+import tornadofx.*
+
+class StatisticsScreen: Fragment("StatisticsScreen") {
+    override val root: Parent by fxml()
+}
