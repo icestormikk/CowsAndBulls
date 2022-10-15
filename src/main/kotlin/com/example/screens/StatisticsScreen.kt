2742f191f1a76d1923ff0cd9c7ec3342572fceba
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-15 23:39:47 +0300

    Added new parameters for statistics

--- src/main/kotlin/com/example/screens/StatisticsScreen.kt
+++ src/main/kotlin/com/example/screens/StatisticsScreen.kt
@@ -2,24 +2,28 @@ package com.example.screens
 
 import com.example.configuration.setEqualWidthForColumns
 import com.example.domain.StatisticsNote
-import com.example.functions.CowsAndBulls
+import com.example.functions.CowsAndBulls.getStatistics
 import javafx.scene.Parent
+import javafx.scene.control.Label
 import javafx.scene.control.TableView
 import javafx.scene.layout.FlowPane
 import tornadofx.*
+import java.util.*
 
 class StatisticsScreen: Fragment("StatisticsScreen") {
     override val root: Parent by fxml()
 
     private val statsContainer: FlowPane by fxid()
+    private val favoriteMode: Label by fxid()
+    private val averageTime: Label by fxid()
 
     init {
-        CowsAndBulls.getStatistics().also { statisticsNotes ->
+        getStatistics().also { statisticsNotes ->
             statsContainer.add(
                 tableview(statisticsNotes.asObservable()) {
                     readonlyColumn("Время начала игры", StatisticsNote::startDateTimeAsString)
                     readonlyColumn("Режим игры", StatisticsNote::gamemodeTitle)
-                    readonlyColumn("Продолжительность игры", StatisticsNote::durationOfGameAsString)
+                    readonlyColumn("Длительность игры", StatisticsNote::durationAsBeautyString)
                     readonlyColumn("Кол-во перезапусков", StatisticsNote::restartsCount)
                 }.apply {
                     prefWidthProperty().bind(statsContainer.widthProperty())
@@ -31,6 +35,17 @@ class StatisticsScreen: Fragment("StatisticsScreen") {
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun causeStatsTableUpdate() =
+    fun causeStatsTableUpdate() {
         (statsContainer.children[0] as TableView<StatisticsNote>).refresh()
+        with(getStatistics()) {
+            if (isNotEmpty()) {
+                favoriteMode.text = groupBy { it.gamemodeTitle }.maxBy { it.value.size }.key
+                averageTime.text = String.format(
+                    Locale.ENGLISH,
+                    "%6f сек.",
+                    map { it.durationOfGameInSeconds }.average()
+                )
+            }
+        }
+    }
 }
