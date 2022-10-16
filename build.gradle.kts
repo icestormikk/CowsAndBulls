1d8b3101db473893d2a2fc5abb68e79437f6e8f9
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-16 11:38:43 +0300

    Adding settings for Jar file assembly

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 javafx {
     modules = listOf("javafx.controls", "javafx.media", "javafx.fxml")
@@ -37,4 +37,22 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    archiveBaseName.set("${project.name}-fat")
+    manifest {
+        attributes["Implementation-Title"] = "CowsAndBulls Kotlin Application"
+        attributes["Implementation-Version"] = "1.0"
+        attributes["Main-Class"] = "com.example.MainKt"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
